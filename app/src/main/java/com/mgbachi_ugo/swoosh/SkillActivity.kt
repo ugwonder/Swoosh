6abd2b8a0dc3459510fb9c86fcceba3ab9f9444a
mgbachi_ugo <ugochukwu_mgbachi@yahoo.com>
2019-11-27 00:17:26 +0100

    lesson 6

---
+++ app/src/main/java/com/mgbachi_ugo/swoosh/SkillActivity.kt
@@ -0,0 +1,12 @@
+package com.mgbachi_ugo.swoosh
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SkillActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_skill)
+    }
+}
